1305746cc7c3b066be61de91eeb428d2d52f9aa5
TianyunLiu <liut2@oregonstate.edu>
2022-10-30 00:15:26 +0000

    modified Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        //.package(url: "git@github.com:<YOUR GITHUB USERNAME>/CrackStation.git", from: "1.0.0")
+        .package(url: "git@github.com:TianyunLiu0529/CrackStation.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
